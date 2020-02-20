ec590f399736245b3b6d0dd18227e1f14bbe007b
Ben Alldridge <ben.alldridge@code42.com>
2020-02-20 15:26:24 +0000

    Added verification for speed argument.

--- pngquant_test.go
+++ pngquant_test.go
@@ -14,13 +14,28 @@ func TestCompress(t *testing.T) {
 	info, _ := file.Stat()
 	orgSize := info.Size()
 	orgImg, _ := png.Decode(file)
+
+	// Test with good data
 	newImg, err := Compress(orgImg, "1")
 	if err != nil {
 		t.Errorf("error has occurred: %v", err)
+	} else {
+		var w bytes.Buffer
+		png.Encode(&w, newImg)
+		if len(w.Bytes()) > int(orgSize) {
+			t.Error("image is not comppressed")
+		}
 	}
-	var w bytes.Buffer
-	png.Encode(&w, newImg)
-	if len(w.Bytes()) > int(orgSize) {
-		t.Error("image is not comppressed")
+
+	// Test with non-numerical data (no need to test encode as we expect nothing)
+	_, err = Compress(orgImg, "one")
+	if err == nil {
+		t.Errorf("ERROR: Expected failure on incompatible speed argument (one)")
+	}
+
+	// Test with number that exceeds the limit (no need to test encode as we expect nothing)
+	_, err = Compress(orgImg, "20")
+	if err == nil {
+		t.Errorf("ERROR: Expected failure on incompatible speed argument (20)")
 	}
 }
